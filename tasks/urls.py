454201cf45afd6f8f07bd7e4ca47e250cc522c2e
Prakritee Karki <prakritikarki940@gmail.com>
2025-11-14 16:37:37 +0545

    TODOAPPMYSELF

---
+++ tasks/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+
+
+
+urlpatterns = [
+
+    path('addtask/',views.addtask,name='addTask'),
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name='mark_as_undone'),
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+   
+    
+
+]
